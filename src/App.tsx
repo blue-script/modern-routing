a57056175ac85b040c756b09ed34b50a3a73716f
demianov-an <Alexm92@inbox.ru>
2023-12-17 17:51:28 +0600

    outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
 import './App.css'
-import { Site } from './components/Site'
-import { Outlet } from 'react-router-dom'
+import styles from './components/Site.module.css'
+import { S } from './components/pages/__styles'
 
 function App() {
 	return (
 		<div>
-            <div>HEADER</div>
-			{/* <Site /> */}
-            <Outlet/>
-            <div>FOOTER</div>
+			<div className={styles.header}>
+				<h1>HEADER</h1>
+			</div>
+			<div className={styles.body}>
+				<div className={styles.nav}>
+					<S.NavWrapper>
+						<NavLink to={'/page/0'}>Page1</NavLink>
+					</S.NavWrapper>
+					<S.NavWrapper>
+						<NavLink to={'/page/1'}>Page2</NavLink>
+					</S.NavWrapper>
+					<S.NavWrapper>
+						<NavLink to={'/page/2'}>Page3</NavLink>
+					</S.NavWrapper>
+					<div>
+						<a href='/page1'>aHrefPage1</a>
+					</div>
+				</div>
+				<div className={styles.content}>
+					<Outlet />
+				</div>
+			</div>
+			<div>FOOTER</div>
 		</div>
 	)
 }
--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import {PagesType} from "../../data/dataState";
-import {Navigate, Route, Routes, useLocation, useParams} from "react-router-dom";
-import {Error404} from "./Error404";
+import React from 'react'
+import { Navigate, useLocation, useParams } from 'react-router-dom'
+import { PagesType } from '../../data/dataState'
 
 type PropsType = {
-    pages: PagesType[]
+	pages: PagesType[]
 }
 
-export const Page: React.FC<PropsType> = ({pages}) => {
-    const params = useParams()
+export const Page: React.FC<PropsType> = ({ pages }) => {
+	const params = useParams()
+	console.log(params)
 
-    const location = useLocation()
-    console.log(location)
+	const location = useLocation()
+	// console.log(location)
 
-    return (
-        <div>
-            {location.pathname === '/page/0' && <div>SECRET TEXT</div>}
-            {pages[Number(params.id)]
-                ? <div>
-                    {pages[Number(params.id)].heading}
-                    {pages[Number(params.id)].about}
-                </div>
-                //: <Error404/>
-                : <Navigate to={'/page/error'}/>
-            }
-
-        </div>
-    );
-};
-
+	return (
+		<div>
+			{location.pathname === '/page/0' && <div>SECRET TEXT</div>}
+			{pages[Number(params.id)] ? (
+				<div>
+					{pages[Number(params.id)].heading}
+					{pages[Number(params.id)].about}
+				</div>
+			) : (
+				//: <Error404/>
+				<Navigate to={'/page/error'} />
+			)}
+		</div>
+	)
+}
