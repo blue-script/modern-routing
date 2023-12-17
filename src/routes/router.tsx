dc7d78586e6201029427ada080246e0f564991fb
demianov-an <Alexm92@inbox.ru>
2023-12-17 16:24:26 +0600

    first commit

---
+++ src/routes/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from './../App'
+import { Error404 } from './../components/pages/Error404'
+import { Page } from './../components/pages/Page'
+import { dataState } from './../data/dataState'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <Error404 />,
+		children: [
+			{
+				path: '/page/:id',
+				element: <Page pages={dataState.pages} />,
+			},
+		],
+	},
+])
